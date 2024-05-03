294e7c9d8329c9bb1051311d7b940af094d6a37b
Luan Tedesco <luanfelipe2040@gmail.com>
2024-05-03 14:59:28 -0300

    added filters

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,14 @@ class TasksController < ApplicationController
 
   def index
     @tasks = Task.all
+
+    if params[:search].present?
+      @tasks = @tasks.where("title LIKE ?", "%#{params[:search]}%")
+    end
+
+    if params[:finished].present?
+      @tasks = @tasks.where(finished: params[:finished])
+    end
   end
 
   def show
@@ -17,45 +25,30 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
+    return unless @task.save
 
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to task_url(@task), notice: "Task was successfully created." }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
-    end
+    redirect_to tasks_path
   end
 
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to task_url(@task), notice: "Task was successfully updated." }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
-    end
+    return unless @task.update(task_params)
+
+    redirect_to tasks_path
   end
 
   def destroy
     @task.destroy
 
-    respond_to do |format|
-      format.html { redirect_to tasks_url, notice: "Task was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to tasks_path
   end
 
   private
-    def set_task
-      @task = Task.find(params[:id])
-    end
 
-    def task_params
-      params.require(:task).permit(:title, :description, :status)
-    end
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :finished)
+  end
 end
